05a077be7edeed5ed7b9cc37e468895e8eaa6e68
anubarukula12 <35746480+anubarukula12@users.noreply.github.com>
2022-05-05 23:23:24 -0600

    Delete passport.controller.js

--- controllers/passport.controller.js
+++
@@ -1,5 +0,0 @@
-
-// require("dotenv").config();
-// const passport=require("passport");
-// const googleclientid=process.env.GOOGLE_CLIENT_ID;
-// const googleclientsecret=process.env.GOOGLE_CLIENT_SECRET;
